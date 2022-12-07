09d6c8c0e362ef419001b10d84cf4cefb01dbd6d
Abhishek2291 <abhishekgajera2017@gmail.com>
2022-12-07 08:19:22 +0530

    fix: text

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div>Header</div>
+      <div>This is Header Component</div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
